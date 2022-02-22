341d31c112e7b136a728307156c3f1be58031b04
Alexander Steen <alexander.steen@vdabcampus.be>
2022-02-22 10:54:39 +0100

    Hoofdstuk Controller

---
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package be.vdab.luigi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+class IndexController {
+    @GetMapping("/")
+    public String index() {
+        var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
+        return "<!doctype html><html><title>Hallo</title><body>Goede "
+                + morgenOfMiddag +
+                "</body></html>";
+    }
+}
