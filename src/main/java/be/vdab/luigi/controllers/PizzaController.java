76cf820737e8d4a5e560674e32db392383386b7b
Alexander Steen <alexander.steen@vdabcampus.be>
2022-02-25 09:43:35 +0100

    Hoofdstuk Service en Transactie

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -1,8 +1,8 @@
 package be.vdab.luigi.controllers;
 
-import be.vdab.luigi.domain.Pizza;
 import be.vdab.luigi.exceptions.KoersClientException;
 import be.vdab.luigi.services.EuroService;
+import be.vdab.luigi.services.PizzaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,63 +12,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("pizzas")
 public class PizzaController {
-    private final Pizza[] allePizzas = {new Pizza(1, "Prosciutto", BigDecimal.valueOf(4), true),
-            new Pizza(2, "Margherita", BigDecimal.valueOf(5), false),
-            new Pizza(3, "Calzone", BigDecimal.valueOf(4), false)};
-    private final EuroService euroService;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final EuroService euroService;
+    private final PizzaService pizzaService;
 
-    public PizzaController(EuroService euroService) {
+    public PizzaController(EuroService euroService, PizzaService pizzaService) {
         this.euroService = euroService;
+        this.pizzaService = pizzaService;
     }
 
     @GetMapping
     public ModelAndView findAll() {
-        return new ModelAndView("pizzas", "allePizzas", allePizzas);
-    }
-
-    private Optional<Pizza> findByIdHelper(long id) {
-        return Arrays.stream(allePizzas).filter(pizza -> pizza.getId() == id).findFirst();
+        return new ModelAndView("pizzas", "allePizzas", pizzaService.findAll());
     }
 
     @GetMapping("{id}")
     public ModelAndView findById(@PathVariable long id) {
         var modelAndView = new ModelAndView("pizza");
-        findByIdHelper(id).ifPresent(gevondenPizza -> {
-            modelAndView.addObject(gevondenPizza);
+        pizzaService.findById(id).ifPresent(pizza -> {
+            modelAndView.addObject("pizza", pizza);
             try {
-                modelAndView.addObject("inDollar", euroService.naarDollar(gevondenPizza.getPrijs()));
+                modelAndView.addObject("inDollar", euroService.naarDollar(pizza.getPrijs()));
             } catch (KoersClientException e) {
                 logger.error("Kan dollar koers niet lezen", e);
-                e.printStackTrace();
             }
         });
         return modelAndView;
     }
 
-    private Stream<BigDecimal> findPrijzenHelper() {
-        return Arrays.stream(allePizzas).map(Pizza::getPrijs).distinct().sorted();
-    }
-
     @GetMapping("prijzen")
     public ModelAndView prijzen() {
-        return new ModelAndView("pizzasperprijs", "prijzen", findPrijzenHelper().iterator());
-    }
-
-    private Stream<Pizza> findByPrijsHelper(BigDecimal prijs) {
-        return Arrays.stream(allePizzas).filter(pizza -> pizza.getPrijs().compareTo(prijs) == 0);
+        return new ModelAndView("pizzasperprijs", "prijzen", pizzaService.findUniekePrijzen());
     }
 
     @GetMapping("prijzen/{prijs}")
     public ModelAndView findByPrijs(@PathVariable BigDecimal prijs) {
-        return new ModelAndView("pizzasperprijs", "pizzas", findByPrijsHelper(prijs).iterator())
-                .addObject("prijzen", findPrijzenHelper().iterator());
+        return new ModelAndView("pizzasperprijs", "pizzas", pizzaService.findByPrijs(prijs))
+                .addObject("prijzen", pizzaService.findUniekePrijzen());
     }
 }
---
+++ src/test/java/be/vdab/luigi/services/PizzaServiceTest.java
@@ -0,0 +1,36 @@
+package be.vdab.luigi.services;
+
+import be.vdab.luigi.domain.Pizza;
+import be.vdab.luigi.repositories.PizzaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PizzaServiceTest {
+    @Mock
+    private PizzaRepository repository;
+    private PizzaService service;
+
+    @BeforeEach
+    void beforeEach() {
+        service = new PizzaService(repository);
+    }
+
+    @Test
+    void create() {
+        var pizza = new Pizza(0, "test", BigDecimal.TEN, false);
+        when(repository.create(pizza)).thenReturn(1L);
+        assertThat(service.create(pizza)).isEqualTo(1L);
+        verify(repository).create(pizza);
+    }
+}
